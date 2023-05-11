6ae7a327f68e3e99882932a0639e97f23ec35389
digitalFlag <gelpostbox@gmail.com>
2023-05-11 06:51:43 +0300

    Code Instance Model has been created.

---
+++ Models/Code/CodeInstance.cs
@@ -0,0 +1,20 @@
+﻿namespace ABCoder.Models.Code
+{
+    class CodeInstance
+    {
+        public string? Name { get; set; }
+
+        public int CodeWordLength { get; set; }
+
+        public int InformationPartLength { get; set; }
+
+        public int VerificationPartLength { get; set; }
+
+        public int ErrorDetectionCapability { get; set; }
+
+        public int ErrorCorrectionCapability { get; set; }
+
+        public string? Polynomial { get; set; }
+
+    }
+}
