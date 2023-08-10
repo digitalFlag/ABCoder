954a7ce4485eed036a9c0d0b7651ac1968a2d569
digitalFlag <gelpostbox@gmail.com>
2023-08-10 06:34:50 +0300

    Ralease 1.3.0 has ben created.

--- ViewModels/MwvmDependencyProperties.cs
+++ ViewModels/MwvmDependencyProperties.cs
@@ -194,7 +194,7 @@ namespace ABCoder.ViewModels
 
         #region Main Window Title
 
-        private string _MainWindowTitle = "ABCoder 1.2.7";
+        private string _MainWindowTitle = "ABCoder 1.3.0";
         /// <summary>Main Window Title</summary>
 
         public string MainWindowTitle
